c962aaafb748d1fcd16ff9d13f23fec2d3562f4c
main()function <madhavkohli37@gmail.com>
2026-02-28 15:30:30 +0530

    Added TreeNode header file

---
+++ Header_files/TreeNode.h
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+template<typename T>
+class TreeNode {
+public:
+	T val;
+	TreeNode *left;
+	TreeNode *right;
+
+	TreeNode() {
+		left = nullptr;
+		right = nullptr;
+	}
+
+	TreeNode(T x) {
+		val = x;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+
