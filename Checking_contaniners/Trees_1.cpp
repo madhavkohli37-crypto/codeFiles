1e13a11c853c955ff83cc71e1fd51a3deefd640c
main()function <madhavkohli37@gmail.com>
2026-03-02 17:02:58 +0530

    Update Trees_1.cpp

--- Checking_contaniners/Trees_1.cpp
+++ Checking_contaniners/Trees_1.cpp
@@ -1,6 +1,25 @@
-#include"../Header_files/Tree.h"
+#include"Tree.h"
 int main() {
   Tree<int> t;
   t.buildTree();
-  cout<<t.root->left->value;
+  t.preorder(t.root);
+  cout<<endl;
+  t.inorder(t.root);
+  cout<<endl;
+  t.postorder(t.root);
+  cout<<endl;
+  cout<<t.height(t.root)<<endl;
+  cout<<t.count_nodes(t.root)<<endl;
+  cout<<t.sumofNodes(t.root)<<endl;
+  t.invertTree(t.root);
+  t.preorder(t.root);
+  cout<<endl;
+  cout<<t.Diameter1(t.root)<<endl;//slower
+  cout<<t.Diameter2(t.root)<<endl;//faster
+  cout<<t.Diameter3(t.root).first<<" "<<t.Diameter3(t.root).second<<endl;//faster and returns pair
+  cout<<t.HBT(t.root).first<<" "<<t.HBT(t.root).second<<endl;
+  int a[]{1,2,3,4,5,6,7,8,9,10};
+  TreeNode<int>*ROOT=t.createHBT(a,0,9);
+  t.preorder(ROOT);
+  
 }
