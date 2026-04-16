db61a1ddd933f05775c97636938b06b349171db4
main()function <madhavkohli37@gmail.com>
2026-04-16 22:04:03 +0530

    Update IPC_Triners_Codechef.cpp

--- practice/IPC_Triners_Codechef.cpp
+++ practice/IPC_Triners_Codechef.cpp
@@ -1,29 +1,19 @@
 #include<iostream>
-
-#include<vector>
-
-#include<map>
-
 #include<set>
-
 #include<queue>
-
-#include<utility>
-
-#include<algorithm>
-
 using namespace std;
 #define int long long
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;
-    cin >> t;
-    while (t--) {
-        int ans = 0;
+    cin>>t;
+    while(t--) {
+        set<int>days;
         int n, d;
         cin >> n >> d;
         priority_queue < pair < pair < int, int > , int >> pq;
+        priority_queue < pair < pair < int, int > , int >> pq1;
         for (int i = 0; i < n; i++) {
             int x, y, z;
             cin >> x >> y >> z;
@@ -35,32 +25,38 @@ int32_t main() {
                 y
             });
         }
-        int i = 0;
-        while (i < d) {
-            int available_days;
-            if (i >= -pq.top().first.second) {
-                available_days = (d - i);
-            }
-            else {
-                available_days = (d + pq.top().first.second + 1);
-            }
-            //    cout<<available_days<<endl;
-            if (available_days >= pq.top().second) {
-                i += pq.top().second;
-                pq.pop();
-            }
-            else {
-                pair < pair < int, int > , int > p = pq.top();
-                p.second = abs(available_days - pq.top().second);
-                pq.pop();
-                pq.push(p);
+        for(int i=1;i<=d;i++) {
+            days.insert(i);
+        }
+       while(!pq.empty()) {
+           if(days.lower_bound(-pq.top().first.second)==days.end()) {
+               pq1.push(pq.top());
+               pq.pop();
+               continue;
+           }
+           set<int>::iterator start=days.lower_bound(-pq.top().first.second);
+        set<int>::iterator end=start;
+        int x=0;
+        int g=pq.top().second;
+        while(g--) {
+            if(end==days.end()) {
                 break;
             }
+            x++;
+           end = days.erase(end);
         }
-        while (!pq.empty()) {
-            ans += ((pq.top().first.first) * (pq.top().second));
-            pq.pop();
+        if(x<pq.top().second) {
+            pair<pair<int,int>,int> p=pq.top();
+            p.second-=x; 
+            pq1.push(p);
         }
-        cout << ans << endl;
+        pq.pop(); 
+       }
+       int ans=0;
+       while( !pq1.empty()) {
+           ans+=(pq1.top().first.first)*(pq1.top().second);
+           pq1.pop();
+       }
+       cout<<ans<<endl;
     }
 }
