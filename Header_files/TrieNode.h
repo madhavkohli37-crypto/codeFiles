7da41038d782b8f076009e4060e558db5fe0da53
main()function <madhavkohli37@gmail.com>
2026-04-25 13:01:01 +0530

    Update TrieNode.h

--- Header_files/TrieNode.h
+++ Header_files/TrieNode.h
@@ -1,17 +1,19 @@
 #include<iostream>
 #include<map>
-#include<unordered_map>
 using namespace std;
 class TrieNode{
   public:
   char data;
   bool check;
+  int size;
   map<char,TrieNode*>mp;
   TrieNode() {
+    size=0;
     this->data='\0';
     check=false;
   }
   TrieNode(char data) {
+    size=0;
     this->data=data;
     this->check=false;
   }
