59e6d8b579ff2c60c5335370e9fb5c75ac9f731f
main()function <madhavkohli37@gmail.com>
2026-04-25 13:02:08 +0530

    Update Trie.h

--- Header_files/Trie.h
+++ Header_files/Trie.h
@@ -1,34 +1,44 @@
-#include"TrieNode.h"
-class Trie{
-  private:
-  TrieNode*root;
-  public:
-  Trie() {
-  this->root=new TrieNode();
-  }
-  TrieNode*temp;
-  void insert(string s) {
-    temp=this->root;
-    for(char ch:s) {
-      if((temp->mp.count(ch))==0) {
-        TrieNode*n=new TrieNode(ch);
-        temp->mp[ch]=n;
-      }
-      temp=temp->mp[ch];
+#include "TrieNode.h"
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() { this->root = new TrieNode(); }
+    TrieNode* temp;
+    void insert(string s) {
+        temp = this->root;
+        for (char ch : s) {
+            if ((temp->mp.count(ch)) == 0) {
+                TrieNode* n = new TrieNode(ch);
+                temp->mp[ch] = n;
+            }
+            temp = temp->mp[ch];
+            (temp->size)++;
+        }
+        temp->check = true;
     }
-    temp->check=true;
-  }
-  bool search(string str) {
-    temp=this->root;
-    for(char ch:str) {
-      if(temp->mp.count(ch)) {
-        temp=temp->mp[ch];
-      }
-      else {
-        return false;
-      }
+    bool search(string str) {
+        temp = this->root;
+        for (char ch : str) {
+            if (temp->mp.count(ch)) {
+                temp = temp->mp[ch];
+            } else {
+                return false;
+            }
+        }
+        return temp->check;
     }
-    return temp->check;
-  }
-  
+    string SUP(string str) {
+        temp = root;
+        string ans;
+        for (char ch : str) {
+            ans += ch;
+            temp = temp->mp[ch];
+            if (temp->size == 1) {
+                return ans;
+            }
+        }
+        return "";
+    }
 };
