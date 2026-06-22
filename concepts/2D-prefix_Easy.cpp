13d2ca6a4ef341b39f7bf984c922dcc2e0e683b0
main()function <madhavkohli37@gmail.com>
2026-06-22 18:21:33 +0530

    Update 2D-prefix_Easy.cpp

--- concepts/2D-prefix_Easy.cpp
+++ concepts/2D-prefix_Easy.cpp
@@ -2,29 +2,31 @@
 using namespace std;
 #define int long long
 int32_t main() {
-    int a[4][4];
-    for(int i=0;i<4;i++) {
-        for(int j=0;j<4;j++) {
+    int n,m;
+    cin>>n>>m;
+    int a[n][m];
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
             cin>>a[i][j];
         }
     }
-    int prefix[4][4];
+    int prefix[n][m];
     
-    for(int i=0;i<4;i++) {
+    for(int i=0;i<n;i++) {
         prefix[i][0]=a[i][0];
     }
-    for(int i=0;i<4;i++) {
-        for(int j=1;j<4;j++) {
+    for(int i=0;i<n;i++) {
+        for(int j=1;j<m;j++) {
             prefix[i][j]=prefix[i][j-1]+a[i][j];
         }
     }
-    for(int i=1;i<4;i++) {
-        for(int j=0;j<4;j++) {
+    for(int i=1;i<n;i++) {
+        for(int j=0;j<m;j++) {
             prefix[i][j]+=prefix[i-1][j];
         }
     }
-    for(int i=0;i<4;i++) {
-        for(int j=0;j<4;j++) {
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
             cout<<prefix[i][j]<<" ";
         }
         cout<<endl;
