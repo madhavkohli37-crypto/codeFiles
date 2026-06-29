f2e01ddc3e903c5653652403db3bcbb2c4e0e33c
main()function <madhavkohli37@gmail.com>
2026-06-29 20:04:54 +0530

    Create BFS_in_graphs.cpp

---
+++ Graphs/BFS_in_graphs.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<algorithm>
+#include<climits>
+#include<map>
+#include<list>
+#include<utility>
+#include<queue>
+#include<vector>
+using namespace std;
+#define int long long
+// we are creating undirected/directed unweighted graph
+template<typename T>
+class Graph{
+private:
+map<T,bool>visited;
+queue<T>q;
+map<T,list<T>>mp;
+public:
+void addEdge(T x,T y,bool direction) {
+    mp[x].push_back(y);
+    if(!direction) {
+        mp[y].push_back(x);
+    }
+    return;
+}
+// print the adjancey list 
+void print() {
+    for(pair<int,list<int>> it:mp) {
+        cout<<(it).first<<" --> ";
+        for(int x:(it).second) {
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
+void bfs(T value) {
+    visited[value]=true;
+    this->q.push(value);
+    
+    while(!q.empty()) {
+        int z=q.front();
+        q.pop();
+        for(int x:mp[z]) {
+            if(!visited[x]) {
+                q.push(x);
+            }
+            visited[x]=true;
+        }
+        cout<<z<<" ";
+    }
+}
+};
+int32_t main() {
+    int m,n;
+    cin>>m>>n;
+    bool direction=0;
+    Graph<int> gh;
+    for(int i=0;i<m;i++) {
+        int x,y;
+        cin>>x>>y;
+        gh.addEdge(x,y,direction);
+    }
+    gh.print();
+    cout<<endl;
+    gh.bfs(1);
+}
+
+   
