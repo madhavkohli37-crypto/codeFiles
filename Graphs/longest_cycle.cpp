4ce575020707a981d8fbc1fa424d957e779a76ed
main()function <madhavkohli37@gmail.com>
2026-07-09 18:43:41 +0530

    Update longest_cycle.cpp

--- Graphs/longest_cycle.cpp
+++ Graphs/longest_cycle.cpp
@@ -3,7 +3,11 @@ private:
     vector<bool> visited = vector<bool>(100005, false);
     vector<int> distance = vector<int>(100005, -1);
     int single_cycle(int& node, vector<int>& mp) {
-        unordered_map<int,bool>visited1;
+        unordered_map<int,bool>visited1;//we have used aunordered map here because of 2 reasons :-
+        // 1). if we use a vector then we have to initialise the size which will make the computation o(n^2) which will give TLE hence we use unordered map in which only the required
+        // keys will be initialised 
+        // 2). The distance is initialised outside the single_cycle function because we cannot initialise it through an unordered map because its every uninitiaslised key has a value 0 which we
+        // do not want because if the cycle ends and start on the same node it will be a problem (we have to identify an uninitialised key as -1 not 0 because 0 is a possible distance).
         int i = 0;
         queue<int> q;
         q.push(node);
