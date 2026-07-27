2f2d4eb3c731f310ca1e9f5c2f5bea10b205d52f
main()function <madhavkohli37@gmail.com>
2026-07-27 20:11:56 +0530

    Update factorial.cpp

--- Maths/factorial.cpp
+++ Maths/factorial.cpp
@@ -1,39 +1,81 @@
-#include<bits/stdc++.h>
+/*
+Concepts we will be studying :-
+1). Modulus
+2). Fast exponent
+3). Euclid
+4). Extended Euclid
+5). MMI(Multiplicative Modulo Inverse).
+6). Modulo Under Division
+7). Fermat's Little Theorem
+8). Factorial 
+9). Factorial under Modulus.
+10). Prime number
+*/
+#include<iostream>
+#include<vector>
 using namespace std;
 #define int long long
-int mod=1e9+7;
-bool isPrime(long long n) {
-    if (n < 2)
+const int mod=1e9+7;
+int isPrime(int n) {
+    if(n<2) {
         return false;
-    if (n == 2)
-        return true;
-    if (n % 2 == 0)
-        return false;
-
-    for (long long i = 3; i * i <= n; i += 2) {
-        if (n % i == 0)
-            return false;
     }
-
+    if(n==2) {
+        return true;
+    }
+    if(n%2==0) {
+        return false;
+    }
+    for(int i=3;i*i<=n;i+=2) {
+        if(n%i==0) {
+            return false;
+        }
+    }
     return true;
 }
-int fastexpo(int a,int exp) {
-    int base=a;
-    int ans=1;
-    while(exp) {
-        if(exp&1) {
-            ans=(ans%mod * base%mod)%mod;
-        }
-        exp=(exp>>1);
-        base=(base%mod * base%mod)%mod;
+    int Modulus_subtraction(int x,int y) {
+        int ans = (x%mod - y%mod + mod) % mod;
+        return ans;
     }
-    return ans;
-}
-vector<int> extendedEuclid(int a, int b) {
+    int Modulus_addition(int x,int y) {
+        int ans = (x%mod + y%mod) % mod;
+        return ans;
+    }
+   int Modulus_Multiplication(int x,int y) {
+        int ans = (x%mod * y%mod) % mod;
+        return ans;
+    }
+    int fastexponent(int a,int exp) {
+        int ans=1;
+        int base=a;
+        while(exp) {
+            if(exp&1) {
+                ans = Modulus_Multiplication(ans,base);
+            }
+            base = Modulus_Multiplication(base,base);
+            exp = exp>>1;
+        } 
+        return ans;
+    }
+    int gcd(int a,int b) {
+        // GCD using euclid dision lemma
+        while(b!=0) {
+            int c = a;
+             a = b;
+             b = c%a;
+        }
+        return a;
+    }
+    vector<int> extendedGCD(int a, int b) {
+        // lets say it returns a vector v
+        // v[0]=gcd of a,b;
+        // for eqution ax+by=gcd;
+        // v[1]=x; which is the Multiplicative modulo inverse of a under modulus mod
+        // v[2]=y; which is the Multiplicative modulo inverse of b under modulus mod
     if (b == 0)
         return {a, 1, 0};
 
-    vector<int> temp = extendedEuclid(b, a % b);
+    vector<int> temp = extendedGCD(b, a % b);
 
     int g = temp[0];
     int x1 = temp[1];
@@ -43,52 +85,52 @@ vector<int> extendedEuclid(int a, int b) {
     int y = x1 - (a / b) * y1;
     return {g, x, y};
 }
-int gcd(int a,int b) {
-    while(b!=0) {
-        int c=a;
-        a=b;
-        b=c%b;
-    }
-    return a;
-}
-int MMI(int a,int p) {
-    // if p (which is the mod) is prime and gcd(p,a)==1 then MMI of a is a^(p-2)%p;
-    // This is the statement of fermat's little theorem(FLT)
-    int mmi=0;
-    if(gcd(a,p)==1) {
-        if(isPrime(p)) {
-            // use fermat's little theorem
-            mmi=fastexpo(a,p-2) ;
+int MMI(int x) {
+    int mmi;
+    if(gcd(x,mod)==1) {
+        if(isPrime(x)) {
+            // MMI is to be calculated by Fermat's Little Theorem.
+            mmi=fastexponent(x,mod-2)%mod ;
         }
         else {
-            mmi=extendedEuclid(a,p)[1];
-            mmi=(mmi%p + p)%p;
+            mmi=extendedGCD(x,mod)[1];
+            mmi=(mmi%mod + mod)%mod;
         }
     }
     else {
-        mmi=-1;
+        // MMI of x does not exists under modulus mod
+        return -1;
     }
     return mmi;
 }
-int nCr(int n, int r, int fact[], int invFact[]) {
-    if (r < 0 || r > n)
-        return 0;
-
-    return (((fact[n]%mod * invFact[r]%mod) % mod) * invFact[n-r]%mod) % mod;
-}
+    int Modulus_Division(int x,int y) {
+        int ans=0;
+        return ans;
+    }
+    int nCr(int n,int r,int facto[],int invfacto[]) {
+        if(r>n) {
+            return -1;
+        }
+        int ans = ((facto[n]%mod * invfacto[n-r]%mod) * facto[r]%mod )%mod;
+        return ans;
+    }
 int32_t main() {
+    cout<<fastexponent(4,5000000000)<<endl;
+    cout<<gcd(4,8)<<endl;
+    cout<<MMI(5)<<endl;
     int n;
     cin>>n;
     int facto[n+1];
+    int invfacto[n+1];
     facto[0]=1;
-    for(int i=1;i<n+1;i++) {
-        facto[i]=(facto[i-1]%mod * i%mod)%mod;
-    }
-    int invfacto[n+1];//this stores the MMI of facto[i]
+    facto[1]=1;
     invfacto[0]=1;
     invfacto[1]=1;
     for(int i=2;i<=n;i++) {
-        invfacto[i]=MMI(facto[i],mod);
+        facto[i]=Modulus_Multiplication(facto[i-1],i);
     }
-    cout<<nCr(5,2,facto,invfacto)<<endl;
+    for(int i=2;i<=n;i++) {
+        invfacto[i]=MMI(facto[i]);
+    }
+    cout<<nCr(5,1,facto,invfacto)<<endl;
 }
