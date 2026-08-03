0e353a1f56f3aadf558fcf407a6d00aa3723efab
main()function <madhavkohli37@gmail.com>
2026-08-04 01:36:02 +0530

    update

--- Sunday Learning/commandlinearguements.cpp
+++ Sunday Learning/commandlinearguements.cpp
@@ -6,3 +6,26 @@ int main(int args,char* argvs[]) {//the format is int , char* [] only
         cout<<argvs[i]<<endl;
     }
 }
+input in terminal g++ (filename).cpp -o (name)
+./(name) arguement_1 arguement_2 ... arguement_n
+output arguements : n+1
+./(name)
+arguement_1
+arguement_2
+.
+.
+arguement_n
+in cmd
+g++ (filename).cpp -o (name)--> to compile
+(filename) direct run 
+output 
+arguements : n+1
+(filename)
+arguement_1
+arguement_2
+.
+.
+arguement_n
+this is because ./(filename) and (filename) are treated as arguements 
+so do
+cout<<"Number of Arguements :"<<args-1<<endl;
